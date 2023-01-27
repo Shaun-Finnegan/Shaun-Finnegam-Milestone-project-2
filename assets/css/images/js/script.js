3c95c132b9bb87247fccad31a9b3de8d16747a09
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-01-27 10:51:37 +0000

    Instruction button has additional functionality and now both opens and closes using seperate buttons and event listeners

--- assets/css/images/js/script.js
+++ assets/css/images/js/script.js
@@ -1,9 +1,19 @@
 const instructionBtn = document.getElementById('instruction-btn');
 const howToPlay = document.getElementById('instructions-text');
+const closeInstructions = document.getElementById('close-instructions');
 
 function showInstructions(){
     howToPlay.style.display = 'block';
+    closeInstructions.style.display = 'block';
+
+};
+
+function hideInstructions(){
+    howToPlay.style.display = 'none';
+    closeInstructions.style.display = 'none';
+
 };
 
 instructionBtn.addEventListener('click', showInstructions);
+closeInstructions.addEventListener('click', hideInstructions);
 
