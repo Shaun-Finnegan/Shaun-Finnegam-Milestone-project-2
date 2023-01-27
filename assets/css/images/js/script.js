22a1634656db632048ac323d5dbf8cc3d6c9eda1
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-01-27 16:08:11 +0000

    background image added to canvas, basic game logic implimented and basic styling has been completed

--- assets/css/images/js/script.js
+++ assets/css/images/js/script.js
@@ -1,8 +1,31 @@
-
-
 const instructionBtn = document.getElementById('instruction-btn');
 const howToPlay = document.getElementById('instructions-text');
 const closeInstructions = document.getElementById('close-instructions');
+const canvas = document.getElementById('canvas-main');
+const ctx = canvas.getContext('2d');
+
+const animationSprite = new Image();
+animationSprite.src = 'assets/css/images/animation rabbit.png';
+let x =20;
+let y = 20;
+let animationSpeed = 5;
+const canvasWidth = 1000;
+const canvasHeight = 300;
+
+function animationLoop(){
+    clearAnimationScreen();
+    ctx.drawImage(animationSprite, 0, 0, 100, 100, x, y, 100, 100);
+    moveAnimationSprite();
+    requestAnimationFrame(animationLoop);
+}
+
+function moveAnimationSprite(){
+   x++
+};
+
+function clearAnimationScreen(){
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+};
 
 function showInstructions(){
     howToPlay.style.display = 'block';
@@ -19,3 +42,4 @@ function hideInstructions(){
 instructionBtn.addEventListener('click', showInstructions);
 closeInstructions.addEventListener('click', hideInstructions);
 
+animationLoop();
