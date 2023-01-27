3775e1eb3296806b0498a591327cf6d0601c2f79
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-01-27 10:36:39 +0000

    instructions added and button interactivity added by using event listeners in JS

--- assets/css/images/js/script.js
+++ assets/css/images/js/script.js
@@ -1,1 +1,9 @@
-console.log('connected');
+const instructionBtn = document.getElementById('instruction-btn');
+const howToPlay = document.getElementById('instructions-text');
+
+function showInstructions(){
+    howToPlay.style.display = 'block';
+};
+
+instructionBtn.addEventListener('click', showInstructions);
+
