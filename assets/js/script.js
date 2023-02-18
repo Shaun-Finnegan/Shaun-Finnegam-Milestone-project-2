e63d649b6d966904d19a3c1739946d5526198ac3
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-02-18 14:44:19 +0000

    comments added to all documentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
+//getting html elements and storing them in variables so that they 
+// can be manipulated
 const instructionBtn = document.getElementById('instruction-btn');
 const howToPlay = document.getElementById('instructions-text');
 const closeInstructions = document.getElementById('close-instructions');
-
+// This function is attached to an event listener so when the how to play
+// button is clicked the game instructions become visible
 function showInstructions(){
     howToPlay.style.display = 'block';
     closeInstructions.style.display = 'block';
-
 }
-
+// this function is also attached to an event listener and when the close button is
+// clicked the game instructions disappear
 function hideInstructions(){
     howToPlay.style.display = 'none';
     closeInstructions.style.display = 'none';
-
 }
 
 instructionBtn.addEventListener('click', showInstructions);
